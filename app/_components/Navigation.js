4252642bfa8b4ffd9c19ae28fb8f336dec75249e
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-08-21 23:15:57 +0300

    feat: add Header, Navigation components

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,21 +1,26 @@
-import Link from "next/link";
-
-function Navigation() {
-    return (
-        <ul>
-            <li>
-                <Link href="/">Home</Link>
-            </li>
-            <li>
-                <Link href="/cabins">Cabins Page</Link>
-            </li>
-            <li>
-                <Link href="/account">Account Page</Link>
-            </li>
-            <li>
-                <Link href="/about">about Page</Link>
-            </li>
-        </ul>
-    );
+export default function Navigation() {
+  return (
+    <nav className="z-10 text-xl">
+      <ul className="flex gap-16 items-center">
+        <li>
+          <a href="/cabins" className="hover:text-accent-400 transition-colors">
+            Cabins
+          </a>
+        </li>
+        <li>
+          <a href="/about" className="hover:text-accent-400 transition-colors">
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="/account"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Guest area
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
-export default Navigation;
