7941373d9c2ca764bd69e1510393b19300cd5018
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-08-22 17:28:45 +0300

    feat: optimize image

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,20 @@
 function Page(){
     return (
-        <div>
-            About Page
+                <Image
+                    src={about1}
+                    alt="Family sitting around a fire pit in front of cabin"
+                />
+            </div>
+
+            {/* What to do if can't statically import image and don't want to set width and height */}
+            <div className="col-span-2 relative  aspect-square">
+                <Image 
+                src="/about-2.jpg" 
+                alt="Family that manages The Wild Oasis"
+                fill
+                className="object-cover object-center"
+                />
+            </div>
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -4,9 +4,14 @@ import Link from "next/link";
 
 function Page() {
   return (
-    <div>
-      <h1>Welcome to wild Oasis</h1>
-    </div>
+      <Image
+        src={bg}
+        fill
+        placeholder="blur"
+        quality={80}
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+      />
   );
 }
 export default Page;
