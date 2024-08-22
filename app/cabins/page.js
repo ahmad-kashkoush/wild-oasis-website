3369da50d9a4dcf2b638c5df1d9e0702f0b062fe
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-08-22 21:56:57 +0300

    featw: add cabins loader

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,13 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "@/app/_lib/data-service";
 
 export const metadata = {
     title: "Cabins"
 }
-export default function Page() {
+export default async function Page() {
     // CHANGE
-    const cabins = [];
+    const cabins = await getCabins();
+
 
     return (
         <div>
@@ -16,7 +18,7 @@ export default function Page() {
                 Cozy yet luxurious cabins, located right in the heart of the Italian
                 Dolomites. Imagine waking up to beautiful mountain views, spending your
                 days exploring the dark forests around, or just relaxing in your private
-                hot tub under the stars. Enjoy nature's beauty in your own little home
+                hot tub under the stars. Enjoy nature&apos; s beauty in your own little home
                 away from home. The perfect spot for a peaceful, calm vacation. Welcome
                 to paradise.
             </p>
