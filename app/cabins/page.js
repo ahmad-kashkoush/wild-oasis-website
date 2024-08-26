b89c63c2cc502cd01b697a7cf4e3d948de40faba
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-08-26 22:32:54 +0300

    feat: fetch cabin data
    used suspense

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,13 @@
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabins } from "@/app/_lib/data-service";
+import Spinner from "@/app/_components/Spinner";
+import CabinsList from "@/app/cabins/CabinsList";
+import { Suspense } from "react";
 
 export const metadata = {
     title: "Cabins"
 }
-export default async function Page() {
+export default function Page() {
     // CHANGE
-    const cabins = await getCabins();
+
 
 
     return (
@@ -22,14 +23,11 @@ export default async function Page() {
                 away from home. The perfect spot for a peaceful, calm vacation. Welcome
                 to paradise.
             </p>
+            <Suspense fallback={<Spinner />}>
+                <CabinsList />
+            </Suspense>
 
-            {cabins.length > 0 && (
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-                    {cabins.map((cabin) => (
-                        <CabinCard cabin={cabin} key={cabin.id} />
-                    ))}
-                </div>
-            )}
         </div>
     );
 }
+
