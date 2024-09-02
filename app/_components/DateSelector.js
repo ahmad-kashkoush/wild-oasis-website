40286c1a2dfca87d40cc663147b8a05e01635b87
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-09-02 18:23:36 +0300

    feat: fetching cabin data
    - optimized data fetching using promise.all() and streaming

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,7 +1,8 @@
+"use client"
 import { isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
-
+import "@/app/_styles/globals.css"
 function isAlreadyBooked(range, datesArr) {
   return (
     range.from &&
@@ -12,22 +13,18 @@ function isAlreadyBooked(range, datesArr) {
   );
 }
 
-function DateSelector() {
+function DateSelector({ settings, cabin }) {
   // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+  const { regularPrice, discount, numNights, cabinPrice } = cabin;
   const range = { from: null, to: null };
 
   // SETTINGS
-  const minBookingLength = 1;
-  const maxBookingLength = 23;
+  const { minBookingLength, maxBookingLength } = settings;
 
   return (
     <div className="flex flex-col justify-between">
       <DayPicker
-        className="pt-12 place-self-center"
+        className=" place-self-center rdp"
         mode="range"
         min={minBookingLength + 1}
         max={maxBookingLength}
@@ -38,7 +35,7 @@ function DateSelector() {
         numberOfMonths={2}
       />
 
-      <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
+      <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px] ">
         <div className="flex items-baseline gap-6">
           <p className="flex gap-2 items-baseline">
             {discount > 0 ? (
