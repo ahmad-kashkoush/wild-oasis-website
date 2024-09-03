a2cfefdfb36958ab7de8d14f3984f8435360c39c
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-09-03 21:45:42 +0300

    feat: setup nextAuth

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,10 @@
+import { auth } from "@/app/_lib/auth";
 import Link from "next/link";
 
-export default function Navigation() {
+export default async function Navigation() {
+  // convert route to dynamic because it is a cookie
+  const session = await auth();
+  console.log(session);
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -15,12 +19,25 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ?
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors  flex gap-4 items-center"
+            >
+              <img
+                src={session.user.image}
+                className="h-8 w-8 rounded-full"
+                alt="user image"
+              />
+              <span>Guest area</span>
+            </Link>
+            : <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          }
         </li>
       </ul>
     </nav>
---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,3 @@
+// this path name means the following
+// app/auth/whatever/ : will be handled by route.js file
+export { GET, POST } from "@/app/_lib/auth";
