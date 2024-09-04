bd1922288ad011aa01c0392eac2d39fd2d9de389
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-09-04 18:38:25 +0300

    feat: add signin/signout server actions

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -4,7 +4,6 @@ import Link from "next/link";
 export default async function Navigation() {
   // convert route to dynamic because it is a cookie
   const session = await auth();
-  console.log(session);
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
--- app/account/page.js
+++ app/account/page.js
@@ -1,8 +1,16 @@
+import { auth } from "@/app/_lib/auth";
+
 // static
-function Page() {
+
+export const metadata={
+    title: "Login"
+}
+async function Page() {
+    const session=await auth();
+   
     return (
         <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-            Welcome, Ahmed
+            Welcome, {session?.user?.name}
         </h2>
 
     );
